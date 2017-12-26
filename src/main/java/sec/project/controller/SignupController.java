c278e1b62edc30afd1a20bbaf2b4811a69da4c41
Veera Koskinen <veera.koskinen@ren.to>
2017-12-26 19:35:22 +0200

    added listing with owner details

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,15 +1,23 @@
 package sec.project.controller;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import sec.project.domain.Account;
 import sec.project.domain.Signup;
+import sec.project.repository.AccountRepository;
 import sec.project.repository.SignupRepository;
 
 @Controller
 public class SignupController {
+    
+    @Autowired 
+    private AccountRepository accountRepository;
 
     @Autowired
     private SignupRepository signupRepository;
@@ -25,9 +33,25 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new Signup(name, address));
+    public String submitFormAndListParticipants(Authentication authentication, @RequestParam String firstname, @RequestParam String familyname, @RequestParam String address, Model model) {
+        
+        // saving new signups --------
+        Account account = accountRepository.findByUsername(authentication.getName());
+        System.out.println("Account: " + account.getUsername());
+        model.addAttribute("currentUsername", account.getUsername());
+        signupRepository.save(new Signup(firstname, familyname, address, account.getUsername()));
+        System.out.println("repositoriossa alkioita: " + signupRepository.findAll().size());
+        // ---------------------------
+        
+        
+        // listing participants ------
+        ArrayList<Signup> participants = (ArrayList) signupRepository.findAll();
+        System.out.println("Listassa osallistujia: " + participants.size());
+        model.addAttribute("participantList", participants);
+        // ---------------------------
+        
+        
         return "done";
     }
-
+    
 }
