59b7aa909c0b7bd89e03050db4a2d8b0a860714a
Veera Koskinen <veera.koskinen@ren.to>
2017-12-26 19:31:00 +0200

    added owner

--- src/main/java/sec/project/domain/Signup.java
+++ src/main/java/sec/project/domain/Signup.java
@@ -6,33 +6,58 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Entity
 public class Signup extends AbstractPersistable<Long> {
 
-    private String name;
+    private String firstname;
+    private String familyname;
     private String address;
+    private String account;
 
     public Signup() {
         super();
     }
 
-    public Signup(String name, String address) {
+    public Signup(String firstname, String familyname, String address, String account) {
         this();
-        this.name = name;
+        this.firstname = firstname;
+        this.familyname = familyname;
         this.address = address;
+        this.account = account;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstname(String name) {
+        this.firstname = name;
     }
 
+    public String getFamilyname() {
+        return familyname;
+    }
+
+    public void setFamilyname(String familyname) {
+        this.familyname = familyname;
+    }
+    
     public String getAddress() {
         return address;
     }
-
+    
     public void setAddress(String address) {
         this.address = address;
+    }
+    
+    public String getAccount() {
+        return account;
+    }
+    
+    public void setAccount(String a) {
+        this.account = a;
+    }
+    
+    
+    public String toString() {
+        return firstname + " " + familyname + ", sähköposti: " + address;
     }
 
 }
