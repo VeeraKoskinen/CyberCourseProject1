fc83b57e3fedf98b8bd8be5a8ecc9884dc4f9dc7
Veera Koskinen <veera.koskinen@ren.to>
2017-12-27 21:44:52 +0200

    added import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+# if you wonder wth these are, then visit 
+# https://finland.fi/arts-culture/all-time-top-12-finnish-design-products/
+INSERT INTO Item (name, price) VALUES ('Fanta', 2.5);
+INSERT INTO Item (name, price) VALUES ('Cola', 2.5);
+INSERT INTO Item (name, price) VALUES ('Beer', 4.0);
+INSERT INTO Item (name, price) VALUES ('Water', 0.0);
+INSERT INTO Item (name, price) VALUES ('Vegetarian menu', 28.0);
+INSERT INTO Item (name, price) VALUES ('Meatlovers menu', 35.0);
+INSERT INTO Item (name, price) VALUES ('Fish menu', 32.5);
+
+
+
+        
