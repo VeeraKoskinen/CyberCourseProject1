9d3ef8e2a629199f78abfe5a995b6cea5303ddb5
Veera Koskinen <veera.koskinen@ren.to>
2017-12-28 20:54:33 +0200

    small changes

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -20,11 +20,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // no real security at the moment
-//        http.authorizeRequests()
-//                .anyRequest().permitAll();
-        http.csrf().disable();
 
+        http.csrf().disable();
+        
         http.headers().frameOptions().sameOrigin();
 
         http.authorizeRequests()
