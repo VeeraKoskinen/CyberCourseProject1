0818f4264f8762799c1e9b7da68900612317e45c
Veera Koskinen <veera.koskinen@ren.to>
2017-12-28 20:50:29 +0200

    spoild removing method

--- src/main/java/sec/project/domain/OrderCart.java
+++ src/main/java/sec/project/domain/OrderCart.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sec.project.domain;
 
 import java.io.Serializable;
@@ -12,10 +8,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-/**
- *
- * @author veerakoskinen
- */
 @Component
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class OrderCart implements Serializable {
@@ -31,16 +23,23 @@ public class OrderCart implements Serializable {
         itemCounts.put(product, itemCounts.get(product) + 1);
     }
     
- // this one is now fixed (Only one if-sentence would have been enough, but now there cannot be the same mistake again.
+ 
     public void removeFromCart(Item product) {
-        if (!itemCounts.containsKey(product) || itemCounts.get(product) <= 0l) {
+        if (!itemCounts.containsKey(product)) {
             return;
         }
-
         itemCounts.put(product, itemCounts.get(product) - 1);
-        if (itemCounts.get(product) == 0) {
-            itemCounts.remove(product, itemCounts.get(product));
-        }
+        
+        
+// Only one if-sentence would have been enough, but this way there cannot be the same mistake again.        
+//        if (!itemCounts.containsKey(product) || itemCounts.get(product) <= 0l) {
+//            return;
+//        }
+//
+//        itemCounts.put(product, itemCounts.get(product) - 1);
+//        if (itemCounts.get(product) == 0) {
+//            itemCounts.remove(product, itemCounts.get(product));
+//        }
     }
 
     public Map<Item, Long> getItems() {
